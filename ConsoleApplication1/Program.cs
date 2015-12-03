4dcfc361c6ff6bd86b4c8d833777f5c4f6500bdb
Torbjorn Unnskog <torbjorn.unnskog@e-man.se>
2015-12-03 10:24:51 +0100

    update

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("contdeploy");
+            Console.WriteLine("contdeploy testing");
         }
     }
 }
